d57fb4af7259b89e4b275ccd7af1eb9b5a23a959
Herrera-Brittany <brittanyherrera@yahoo.com>
2019-07-03 11:49:35 -0500

    Add files via upload

---
+++ Find/INFO.sql
@@ -0,0 +1,8 @@
+CREATE TABLE  "INFO"   
+   (    "CONTACTNO" NUMBER,   
+    "NAME" VARCHAR2(40),   
+    "INFO" VARCHAR2(40),   
+    "GRADE" VARCHAR2(40),   
+     CONSTRAINT "INFO_PK" PRIMARY KEY ("CONTACTNO") ENABLE  
+   )  
+/  
